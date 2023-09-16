dbf8c3dabde9b969d303250190f219982955fe75
Gentlestan <81222819+Gentlestan@users.noreply.github.com>
2023-09-16 19:17:56 +0100

    Update Card.jsx
    updated from MoviesDetail/id to movies/id

--- pages/Card.jsx
+++ pages/Card.jsx
@@ -6,7 +6,7 @@ export default function Card(props){
     
     return(
         <div>
-              <Link className="link" to={`/MovieDetails/${props.id}`}>
+              <Link className="link" to={`/movies/${props.id}`}>
              <img data-testid= "movie-poster" src={`https://image.tmdb.org/t/p/w500${props.posterPath}`} alt="Movie Poster" />
              <p data-testid= "movie-release-date">{props.releaseDate}</p>
              <h4 data-testid= "movie-title">{props.title}</h4>
