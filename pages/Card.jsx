01bac63f6a2110b163b4ef7ea3920497093a90f4
Gentlestan <81222819+Gentlestan@users.noreply.github.com>
2023-09-16 19:25:34 +0100

    Update Card.jsx

--- pages/Card.jsx
+++ pages/Card.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 export default function Card(props){
     
     return(
-        <div>
+        <div data-testid= "movie-card" >
               <Link className="link" to={`/movies/${props.id}`}>
              <img data-testid= "movie-poster" src={`https://image.tmdb.org/t/p/w500${props.posterPath}`} alt="Movie Poster" />
              <p data-testid= "movie-release-date">{props.releaseDate}</p>
